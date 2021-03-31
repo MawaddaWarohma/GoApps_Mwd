907723c118ef2ff27882465fc279d674cfc4786a
MawaddaWarohma <11950121716@students.uin-suska.ac.id>
2021-03-31 15:58:16 +0700

    first commit

---
+++ app/src/main/java/com/excample/goapps_mwd/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package com.excample.goapps_mwd.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
